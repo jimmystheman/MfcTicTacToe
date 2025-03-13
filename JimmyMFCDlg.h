5c65a7a5d4ef643ef8328c19566f4449b84901d9
wjh <wjhaviland@outlook.com>
2025-03-13 15:49:13 -0500

    empty MFC app

---
+++ JimmyMFCDlg.h
@@ -0,0 +1,36 @@
+
+// JimmyMFCDlg.h : header file
+//
+
+#pragma once
+
+
+// CJimmyMFCDlg dialog
+class CJimmyMFCDlg : public CDialogEx
+{
+// Construction
+public:
+	CJimmyMFCDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_JIMMYMFC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+};
